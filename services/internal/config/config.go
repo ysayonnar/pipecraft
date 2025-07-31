824c500bde2fe3d737968890f5e264c3aa75d00f
ysayonnar <ysayonnar@gmail.com>
2025-07-31 12:50:52 +0300

    repo init

---
+++ services/internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+const DEFAULT_CONFIG_PATH = "config.yml"
+
+type Config struct {
+	Port int `yaml:"port"`
+}
+
+func MustParse() *Config {
+	return MustParseByPath(DEFAULT_CONFIG_PATH)
+}
+
+func MustParseByPath(cfgPath string) *Config {
+	data, err := os.ReadFile(cfgPath)
+	if err != nil {
+		panic(fmt.Errorf("error while reading config file: %w", err))
+	}
+
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		panic(fmt.Errorf("error while unmarshaling config file: %w", err))
+	}
+
+	return &cfg
+}
