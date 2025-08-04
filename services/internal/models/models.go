3210cf705a56de9ff39af1c514a1475258d91c0a
ysayonnar <ysayonnar@gmail.com>
2025-08-04 22:56:43 +0300

    new: DTO models

--- services/internal/models/models.go
+++ services/internal/models/models.go
@@ -14,3 +14,19 @@ type RunPipelineRequest struct {
 type RunPipelineResponse struct {
 	PipelineId int64 `json:"pipeline_id,omitempty"`
 }
+
+type PipelineStatusResponse struct {
+	Status string `json:"status"`
+}
+
+type Logs struct {
+	LogsId        int64  `json:"logs_id"`
+	Command       string `json:"command_name"`
+	CommandNumber int    `json:"command_number"`
+	Results       string `json:"results"`
+	FinalStatus   string `json:"final_status"`
+	PipelineId    int64  `json:"pipeline_id"`
+}
+type PipelineLogsResponse struct {
+	Logs []Logs `json:"logs"`
+}
