a5233041a66fd7b0dbc39435fd89fba4b005f0c2
ysayonnar <ysayonnar@gmail.com>
2025-08-19 14:47:04 +0300

    upd: methodNotAllowed test for /run-pipeline handler

--- services/internal/handlers/handlers_test.go
+++ services/internal/handlers/handlers_test.go
@@ -47,3 +47,15 @@ func TestHandlers_RunPipeline_HappyPath(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, response.PipelineId, int64(1))
 }
+
+func TestHandlers_RunPipeline_MethodNotAllowed(t *testing.T) {
+	redisMock := NewMockRedisServie()
+	pipelinesMock := NewMockPipelineService()
+	handlers := New(redisMock, pipelinesMock)
+
+	req, _ := http.NewRequest(http.MethodGet, "/run-pipeline", nil)
+	rr := httptest.NewRecorder()
+
+	handlers.RunPipeline(rr, req)
+	require.Equal(t, http.StatusMethodNotAllowed, rr.Code)
+}
